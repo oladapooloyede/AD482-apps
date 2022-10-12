9d36606db6bbd45f1ff355dba37812a66999bd60
oladapooloyede <30477121+oladapooloyede@users.noreply.github.com>
2022-10-12 13:10:06 -0600

    Update Accountant.java

--- connect-debezium/apps/accountants/src/main/java/com/redhat/training/model/Accountant.java
+++ connect-debezium/apps/accountants/src/main/java/com/redhat/training/model/Accountant.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class Accountant extends PanacheEntityBase {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
